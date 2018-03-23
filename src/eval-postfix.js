4c2c159cffc462f246939cf113f67e18e030ba74
sretundijr <sretundijr@gmail.com>
2018-03-23 13:57:14 -0600

    moved evalpostfix to seperate file

---
+++ src/eval-postfix.js
@@ -0,0 +1,31 @@
+// evalulates a postfix equation required format [type Number, string Operators]
+export default class EvalPostfix {
+  constructor() {
+    this.state = {
+      resultsStack: [],
+    };
+  };
+  returnResults(postfixList) {
+    postfixList.forEach((item) => {
+      if (!isNaN(item)) {
+        this.state.resultsStack.push(item);
+      } else {
+        const operand1 = this.state.resultsStack.pop();
+        const operand2 = this.state.resultsStack.pop();
+        let results = 0;
+        // temp till i get back to the internet
+        if (item === '+') {
+          results = operand2 + operand1;
+        } else if (item === '-') {
+          results = operand2 - operand1;
+        } else if (item === '/') {
+          results = operand2 / operand1;
+        } else {
+          results = operand2 * operand1;
+        }
+        this.state.resultsStack.push(results);
+      }
+    })
+    return this.state.resultsStack.join('');
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 
 import InfixToPostfix from './infix-postfix';
+import EvalPostfix from './eval-postfix';
 
 const keyPadMarkup = (value) => {
   return (
@@ -97,39 +98,6 @@ class Calculator {
   //   console.log(decimalIndex);
   // }
 };
-
-// evalulates a postfix equation required format [type Number, string Operators]
-class EvalPostfix {
-  constructor() {
-    this.state = {
-      resultsStack: [],
-    };
-  };
-  returnResults(postfixList) {
-    postfixList.forEach((item) => {
-      if (!isNaN(item)) {
-        this.state.resultsStack.push(item);
-      } else {
-        const operand1 = this.state.resultsStack.pop();
-        const operand2 = this.state.resultsStack.pop();
-        let results = 0;
-        // temp till i get back to the internet
-        if (item === '+') {
-          results = operand2 + operand1;
-        } else if (item === '-') {
-          results = operand2 - operand1;
-        } else if (item === '/') {
-          results = operand2 / operand1;
-        } else {
-          results = operand2 * operand1;
-        }
-        this.state.resultsStack.push(results);
-      }
-    })
-    return this.state.resultsStack.join('');
-  }
-}
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const Calc = new Calculator();
